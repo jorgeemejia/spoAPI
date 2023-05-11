a3941a9facab39b5b06920b58c9fd4fbe25d8a13
Jorge <jorgeemejia@csu.fullerton.edu>
2023-05-11 14:16:22 -0700

    cleaned up mah code, added commented removedprints

--- app.py
+++ app.py
@@ -22,33 +22,42 @@ from Cryptodome.PublicKey import RSA
 from cryptography.exceptions import InvalidSignature
 from datetime import datetime
 
+#Grab the path to the SSL Certificate file
 cert_path = os.path.join(os.getcwd(), 'localhost.pem')
+#Grab the path to the SSL Private Key file
 key_path = os.path.join(os.getcwd(), 'localhost-key.pem')
 
+#Create a Flask application instance
 app = Flask(__name__)
+#Enable Cross-Origin Resource Sharing (CORS)
 CORS(app, resources={r"/*": {"origins": ["https://localhost:3000"]}})
 
+#Establish a connection the MySql database
 db = dbConnection()
 
-
+"""
+The login route 
+(1)Gets the username and password from the request
+(2)Checks if the user actually is an existing member
+(3)Sends a jwt if everything checks out
+"""
 @app.route('/login', methods=['POST'])
 def login():
+    #(1) Gets the username and password from the request
     data = request.get_json()
     username = data['username']
     password = data['password']
+    #(2)Checks if the user actually is an existing member
     cursor = db.cursor()
     cursor.execute("SELECT * FROM users WHERE userName=%s AND userPassword=%s", (username, password))
     user = cursor.fetchone()
+    #(3)Sends a jwt if everything checks out
     if user:
-        userPublic = user[2]
-        #create the jwt token
-        # payload = {'username': username, 'publicKey' : userPublic}
         payload = {'username': username}
         secret_key = 'secretkey123'
         algorithm = 'HS256'
         jwt_token = jwt.encode(payload, secret_key, algorithm=algorithm)
-
-        response = jsonify({'success': True, 'userPublic': userPublic, 'jwt' : jwt_token})
+        response = jsonify({'success': True, 'jwt' : jwt_token})
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     else:
@@ -56,68 +65,45 @@ def login():
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
 
+"""
+The order route 
+(1)Gets the order, timestamp, and digital signature from the request
+(2)Grabs the jwt from the authorization headers and extracts the username within
+   the jwt
+(3)Gets a public key from the db that corresponds to the username
+(4)Verifies the digital signature
+(5)Verifies the timestamp is fresh(5 minutes)
+(6)Checks if the order is in stock
+"""
 @app.route('/order', methods=['POST'])
 def order():
+    #(1)Gets the order, timestamp, and digital signature from the request
     data = request.get_json()
     order = data['order']
     timestamp = data['timestamp']
     signature = data['signature']
-
-    # Extract username from JWT token
+    #(2)Grabs the jwt from the authorization headers and extracts the username within
+    #the jwt
     token = request.headers.get('Authorization')
-    print("Token: ", token)
     if not token:
         return jsonify({'message': 'Authorization token is missing'}), 401
     token = token.split(' ')[1]
-    print("Token: ", token)
     try: 
         payload = jwt.decode(token, 'secretkey123', algorithms=['HS256'])
     except jwt.ExpiredSignatureError:
             return jsonify({'message': 'Expired token'}), 401
     except jwt.InvalidTokenError:
-        return jsonify({'message': 'Invalid token!!!!!!'}), 401
-    
+        return jsonify({'message': 'Invalid token!'}), 401
     username = payload['username']
-
-    
-
-#todo
-#gets passed username so keep debugging down the line
-#i believe the next hitch would be checking if verification of digital signature work
-
-    # if username:
-    #     print(username)
-    #     return jsonify({'message': 'Username is here'}), 401
-
-    # Get public key from username in database.users
+    #(3)Gets a public key from the db that corresponds to the username
     cursor = db.cursor()
     cursor.execute("SELECT userPublic FROM users WHERE userName=%s", (username,))
     publicKey = cursor.fetchone()
-
-    #For some reason a tuple is returned so we just get the first element
     publicKey = publicKey[0]
-    print("publicKey: ", publicKey)
-
-
-    print("Order: ", order)
-
-    # Convert the order string to bytes using UTF-8 encoding
+    #(4)Verifies the digital signature
     order_bytes = order.encode('utf-8')
-    sha256_digest = hashlib.sha256(order_bytes).hexdigest()
-    #THE HEX OF ORDER VALUE RECIEVED AND HEX OF ORDER VALUE ON FRONTEND MATCH
-    print("Order Hex:", sha256_digest)
-
-
-    #Omitted turning public key into an object
-    #Next, unlock the digest in the digital signature
-
-
-        # Decode the base64-encoded signature
     decoded_signature = base64.b64decode(signature)
-
     public_key = serialization.load_pem_public_key(publicKey.encode(), backend=default_backend())
-
-    # Verify the signature and extract the message
     try:
         public_key.verify(
             decoded_signature,
@@ -125,49 +111,28 @@ def order():
             padding.PKCS1v15(),
             hashes.SHA256()
         )
-        message = order_bytes.decode()
-        print("Extracted Message:", message)
         verified = True
     except InvalidSignature:
-        print("Invalid signature")
         verified = False
-
     if not verified:
         return jsonify({'message': 'Invalid signature'}), 401
-    if verified:
-        print("VERIFIED BABY")
-        # return jsonify({'message': 'success'}), 201
-
-#TODO VERIFICATION WORKS
-#NOW TIME TO CHECK OUT TIMESTAMP
-
-
-    # Check timestamp within 5 minutes
-    submitted_timestamp = timestamp[:-1]  # Remove the 'Z' character
-    print("Submitted timestamp:", submitted_timestamp)
-
+    #(5)Verifies the timestamp is fresh(5 minutes)
+    submitted_timestamp = timestamp[:-1]
     current_timestamp = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%f")
-    print("Current timestamp (UTC):", current_timestamp)
-
     submitted_datetime = datetime.strptime(submitted_timestamp, "%Y-%m-%dT%H:%M:%S.%f")
     current_datetime = datetime.strptime(current_timestamp, "%Y-%m-%dT%H:%M:%S.%f")
-
     time_diff = (current_datetime - submitted_datetime).total_seconds()
-
     if time_diff > 300:
         return jsonify({'message': 'Timestamp is invalid'}), 401
-
-
-    # Check if order is in the database (perform your database check here)
+    #(6)Checks if the order is in stock
     cursor.execute("SELECT order_number FROM orders WHERE order_number=%s", (order, ))
     grabbed_order = cursor.fetchone()
     if grabbed_order:
-        response = jsonify({'success': True, "order_status:" : True})
+        response = jsonify({'success': True, "order_status:" : "completed"})
         response.headers.add('Access-Control-Allow-Origin', '*')
-        print('Order was found')
         return response
     else:
-        response = jsonify({'success': False, "order_status:" : False})
+        response = jsonify({'success': False, "order_status:" : "unable to complete"})
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
         
